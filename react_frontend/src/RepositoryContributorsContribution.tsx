883568b273653329e2557feb364e4e638789add4
Chaaand03 <chaaand2001@gmail.com>
2023-11-28 16:05:36 -0500

    bar Graphs and display modifications

--- react_frontend/src/RepositoryContributorsContribution.tsx
+++ react_frontend/src/RepositoryContributorsContribution.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart, LineController, TimeScale, LinearScale, Title, Tooltip, registerables } from "chart.js";
+import { Line } from "react-chartjs-2";
+import { ChartOptions, TooltipItem } from "chart.js";
+import "./styles.css";
+import 'chartjs-adapter-moment';
 
 interface Commit {
   authoredDate: string;
@@ -26,6 +32,9 @@ interface ApiResponse {
 
 const RepositoryContributorsContribution: React.FC= () => {
   const [data, setData] = useState<ApiResponse | null>(null);
+  const [authoredDates, setAuthoredDates] = useState<string[]>([]);
+  const [additions, setAdditions] = useState<number[]>([]);
+  const [deletions, setDeletions] = useState<number[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,15 +45,228 @@ const RepositoryContributorsContribution: React.FC= () => {
 
     fetchData();
   },[]);
-  
+
+  useEffect(() => {
+    if (data) {
+      const commitNodes = data.repository?.defaultBranchRef?.target?.history?.nodes || [];
+
+      const newAuthoredDates = commitNodes.map((commit) => commit.authoredDate);
+      setAuthoredDates(newAuthoredDates);
+
+      const newAdditions = commitNodes.map((commit) => commit.additions);
+      setAdditions(newAdditions);
+
+      const newDeletions = commitNodes.map((commit) => commit.deletions);
+      setDeletions(newDeletions);
+    }
+  }, [data]);
+
+  const chartData = {
+    labels: authoredDates,
+    datasets: [
+      {
+        label: "Additions",
+        data: additions,
+        backgroundColor: "rgba(75,192,192,0.5)", // Green color
+        borderColor: "rgba(75,192,192,1)",
+      },
+      {
+        label: "Deletions",
+        data: deletions,
+        backgroundColor: "rgba(255,99,132,0.5)", // Red color
+        borderColor: "rgba(255,99,132,1)",
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        stacked: true,
+        grid: {
+          display: false,
+        },
+        ticks: {
+          autoSkip: true,
+          maxTicksLimit: 13,
+        },
+      },
+      y: {
+        stacked: true,
+        grid: {
+          color: "#e6e6e6",
+          drawBorder: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+        position: "top",
+      },
+    },
+  };
+
+  // useEffect(() => {
+  //   if (data) {
+  //     const ctx = document.getElementById("myChart") as HTMLCanvasElement;
+  //     if (ctx) {
+  //               // Destroy existing chart instance if it exists
+  //       if (chartRef.current) {
+  //         chartRef.current.destroy()                
+  //       }
+        
+  //       chartRef.current = new Chart(ctx, {
+  //         type: "line",
+  //         data: {
+  //           labels: data.repository?.defaultBranchRef?.target?.history?.nodes.map(
+  //             (commit) => commit.authoredDate
+  //           ) || [],
+  //           datasets: [
+  //             {
+  //               label: "Additions",
+  //               data: data.repository?.defaultBranchRef?.target?.history?.nodes.map(
+  //                 (commit) => commit.additions
+  //               ) || [],
+  //               borderColor: "rgba(75,192,192,1)",
+  //               backgroundColor: "rgba(75,192,192,0.2)",
+  //               fill: false,
+  //             },
+  //           ],
+  //         },
+  //         options: {
+  //           responsive: true,
+  //           maintainAspectRatio: false,
+  //           plugins: {
+  //             title: {
+  //               display: false,
+  //             },
+  //             tooltip: {
+  //               mode: "index",
+  //               intersect: false,
+  //             },
+  //           },
+  //           scales: {
+  //             x: {
+  //               type: "time",
+  //               stacked: true,
+  //               grid: {
+  //                 display: false,
+  //               },
+  //               ticks: {
+  //                 autoSkip: true,
+  //                 maxTicksLimit: 13,
+  //               },
+  //             },
+  //             y: {
+  //               type: "linear",
+  //               stacked: true,
+  //               grid: {
+  //                 color: "#e6e6e6",
+  //               },
+  //             },
+  //           },
+  //         },
+  //       });
+  //     }
+  //   }
+  // }, [data]);
+
+  // const chartData = {
+  //   labels: data?.repository?.defaultBranchRef?.target?.history?.nodes.map(
+  //     (commit) => commit.authoredDate
+  //   ) || [],
+  //   datasets: [
+  //     {
+  //       label: "Additions",
+  //       data: data?.repository?.defaultBranchRef?.target?.history?.nodes.map(
+  //         (commit) => commit.additions
+  //       ) || [],
+  //       backgroundColor: "rgba(75,192,192,0.5)", // Green color
+  //       borderColor: "rgba(75,192,192,1)",
+  //     },
+  //     {
+  //       label: "Deletions",
+  //       data: data?.repository?.defaultBranchRef?.target?.history?.nodes.map(
+  //         (commit) => commit.deletions
+  //       ) || [],
+  //       backgroundColor: "rgba(255,99,132,0.5)", // Red color
+  //       borderColor: "rgba(255,99,132,1)",
+  //     },
+  //   ],
+  // };
+
+  // const chartOptions = {
+  //   responsive: true,
+  //   maintainAspectRatio: false,
+  //   scales: {
+  //     x: {
+  //       type: "time",
+  //       stacked: true,
+  //       grid: {
+  //         display: false,
+  //       },
+  //       ticks: {
+  //         autoSkip: true,
+  //         maxTicksLimit: 13,
+  //       },
+  //     },
+  //     y: {
+  //       stacked: true,
+  //       grid: {
+  //         color: "#e6e6e6",
+  //         drawBorder: false,
+  //       },
+  //     },
+  //   },
+  //   plugins: {
+  //     legend: {
+  //       display: true,
+  //       position: "top",
+  //     },
+  //   },
+  // };
+
   return (
-    <div>
-      {data && data.repository && data.repository.defaultBranchRef && data.repository.defaultBranchRef.target && data.repository.defaultBranchRef.target.history && (
+    // <div>
+    //   {data && data.repository && data.repository.defaultBranchRef && data.repository.defaultBranchRef.target && data.repository.defaultBranchRef.target.history && (
+    //     <div>
+    //       <br></br>
+    //       <h2>Repository specific information</h2><br></br>
+    //       <p>Total Commits: {data.repository.defaultBranchRef.target.history.totalCount}</p>
+    //       <ul>
+    //         {data.repository.defaultBranchRef.target.history.nodes.map((commit, index) => (
+    //           <li key={index}>
+    //             <p>Authored Date: {commit.authoredDate}</p>
+    //             <p>Changed Files If Available: {commit.changedFilesIfAvailable}</p>
+    //             <p>Additions: {commit.additions}</p>
+    //             <p>Deletions: {commit.deletions}</p>
+    //             <p>Commit Message: {commit.message}</p>
+    //             <p>Parents Total Count: {commit.parents.totalCount}</p>
+    //             <hr />
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   )}
+    // </div>
+
+    <div className="commit-cards">
+    {data &&
+      data.repository &&
+      data.repository.defaultBranchRef &&
+      data.repository.defaultBranchRef.target &&
+      data.repository.defaultBranchRef.target.history && (
         <div>
+          <br />
+          <h2>Repository specific information</h2>
+          <br />
           <p>Total Commits: {data.repository.defaultBranchRef.target.history.totalCount}</p>
-          <ul>
+          <div className="commit-cards-container">
             {data.repository.defaultBranchRef.target.history.nodes.map((commit, index) => (
-              <li key={index}>
+              <div key={index} className="commit-card">
                 <p>Authored Date: {commit.authoredDate}</p>
                 <p>Changed Files If Available: {commit.changedFilesIfAvailable}</p>
                 <p>Additions: {commit.additions}</p>
@@ -52,13 +274,20 @@ const RepositoryContributorsContribution: React.FC= () => {
                 <p>Commit Message: {commit.message}</p>
                 <p>Parents Total Count: {commit.parents.totalCount}</p>
                 <hr />
-              </li>
+              </div>
             ))}
-          </ul>
+          </div>
+          {/* <div className="chart-container">
+              <Line data={chartData} options={chartOptions as any} />
+          </div> */}
+          {/* <canvas id="myChart" /> */}
+          <Bar data={chartData} options={chartOptions as any} />
         </div>
       )}
-    </div>
+  </div>
   );
 };
+
+Chart.register(...registerables);
 
 export default RepositoryContributorsContribution;
--- react_frontend/src/UserLogin.tsx
+++ react_frontend/src/UserLogin.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./styles.css"; // Import the CSS file for styling
 
 // Define the shape of the data you expect from the API, adjust as needed.
 interface User {
@@ -27,15 +28,39 @@ const UserLogin: React.FC = () => {
   }, []);
 
   return (
+    // <div>
+    //   {data && (
+    //     <div>
+    //       <p>Login: {data.user.login}</p>
+    //       <p>Name: {data.user.name}</p>
+    //       <p>ID: {data.user.id}</p>
+    //       <p>Email: {data.user.email}</p>
+    //       <p>Created At: {data.user.createdAt}</p>
+    //     </div>
+    //   )}
+    // </div>
     <div>
       {data && (
-        <div>
-          <p>Login: {data.user.login}</p>
-          <p>Name: {data.user.name}</p>
-          <p>ID: {data.user.id}</p>
-          <p>Email: {data.user.email}</p>
-          <p>Created At: {data.user.createdAt}</p>
-        </div>
+        <table className="user-table">
+          <thead>
+            <tr>
+              <th>Login</th>
+              <th>Name</th>
+              <th>ID</th>
+              {/* <th>Email</th> */}
+              <th>Created At</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{data.user.login}</td>
+              <td>{data.user.name}</td>
+              <td>{data.user.id}</td>
+              {/* <td>{data.user.email}</td> */}
+              <td>{data.user.createdAt}</td>
+            </tr>
+          </tbody>
+        </table>
       )}
     </div>
   );
